2af188a540d3f2b2d51f09491b330b532022fd99
Carlos Sebastian Condor Padilla <ccondorp@est.ups.edu.ec>
2025-05-07 22:56:43 -0500

    [ADD] custom_addons

---
+++ custom_addons/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'category': 'Kawiil/Custom Modules',
+    'version': '18.0.0.0.1',
+    'website': 'https://github.com/CarlosCondor99/K-awiil-Motors', 
+    'author': 'CarlosCondor99',
+    'license': 'OPL-1',
+    'application': True,
+    'depends': [],
+    'data': [],
+    'installable': True,
+}
