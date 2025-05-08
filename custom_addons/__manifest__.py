7fe9c283db7eba673d41aa2ca78b70bf9f2fd48b
Carlos Sebastian Condor Padilla <ccondorp@est.ups.edu.ec>
2025-05-08 00:36:24 -0500

    [IMP] custom_addons: new demo data

--- custom_addons/__manifest__.py
+++ custom_addons/__manifest__.py
@@ -5,6 +5,9 @@
     'version': '18.0.0.0.1',
     'website': 'https://github.com/CarlosCondor99/K-awiil-Motors', 
     'author': 'CarlosCondor99',
+    'demo': [
+        'data/loan_demo.xml',
+    ],
     'license': 'OPL-1',
     'application': True,
     'depends': [],
